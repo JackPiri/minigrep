ac5d5aa839d99c6895d3b90b1f1e824fd8686e7b
JackPiri <gpirinoli@horizenlabs.io>
2024-01-26 16:30:30 +0100

    Added grep feature

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use std::env;
+use std::process;
+use minigrep::*;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    match run(config) {
+        Ok(_) => {},
+        Err(err) => {
+            eprintln!("Application error: {}", err);
+            process::exit(1);
+        }
+    };
+}
