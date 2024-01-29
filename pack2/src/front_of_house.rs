5d25018e7cdd073775e0b1cbb4ce5722d1ac9f35
JackPiri <gpirinoli@horizenlabs.io>
2024-01-29 12:32:59 +0100

    pr

--- pack2/src/front_of_house.rs
+++ pack2/src/front_of_house.rs
@@ -2,6 +2,7 @@ pub mod hosting1;
 pub mod hosting2;
 
 pub fn front_of_house_f() {
+    let myvar = 2;
     hosting1::add_to_waitlist1();
     hosting1::hosting1_1::add_to_waitlist1_1();
     hosting2::add_to_waitlist2();
