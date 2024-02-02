b6f4c4ebcadedd32a93bd621d59de29b5ac7777f
GiacomoPirinoli <gpirinoli@horizenlabs.io>
2024-02-02 16:48:06 +0100

    tmp

--- pack2/src/front_of_house.rs
+++ pack2/src/front_of_house.rs
@@ -2,7 +2,7 @@ pub mod hosting1;
 pub mod hosting2;
 
 pub fn front_of_house_f() {
-    let myvar = 8;
+    let myvar = 9;
     let foo = 10;
     println!("foo {}", foo);
     hosting1::add_to_waitlist1();
